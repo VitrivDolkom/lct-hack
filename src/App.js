bb7c502e26fb6c535f7ced3d36ef6076c7568be3
Dmitriy Volkov <vdy0555@gmail.com>
2022-11-06 17:25:54 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
